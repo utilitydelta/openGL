9d43281ebff8dcc64a8edda762b4f88fb7e82caf
utilitydelta <tysonbrown@live.com.au>
2021-06-01 22:01:04 +1000

    error handling

--- p1/chernoOpenGL/src/Application.cpp
+++ p1/chernoOpenGL/src/Application.cpp
@@ -5,6 +5,23 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+	x;\
+	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+static void GLClearError() {
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line) {
+	while (auto err = glGetError()) {
+		std::cout << "OpenGL Error: " << err << " function: " << function << " file: " << file << " line: " << line << std::endl;
+		return false;
+	}
+	return true;
+}
+
 struct ShaderProgramSource {
 	std::string VertexSource;
 	std::string FragmentSource;
@@ -174,7 +191,7 @@ int main(void)
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr)); //DELIBERATE ERROR
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
